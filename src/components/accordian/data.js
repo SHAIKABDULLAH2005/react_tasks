ac9b80fdd7e40b4be07a204765373501b4627d57
Shaik Abdullah <hsshaikabdullah@gmail.com>
2025-11-05 11:00:48 +0530

    Initial commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data=[
+    {
+        id:'1',
+        question: "What is functional components?",
+        answer: "A functional component in react is a simple javascript function that returns JSX"
+    },
+    {
+        id:'2',
+        question:"What is fragments?",
+        answer:"A fragment in react is a special wrapper used to group multiple elements without adding extra node to the DOM"
+    },
+    {
+        id:'3',
+        question:"What is Reconciliation ?",
+        answer:"A reconciliation in react is process of updating the virtual DOM and syncing it with real DOM efficiantly"
+    },
+    {
+        id:'4',
+        question:"What is props?",
+        answer:"A props in react pass data in parent component to child component. "
+    }
+];
+export default data;
